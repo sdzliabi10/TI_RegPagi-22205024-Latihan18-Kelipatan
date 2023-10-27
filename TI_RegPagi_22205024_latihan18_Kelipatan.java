23e32db1c5d481cb999333b6a904ece3d9e41a8c
sdzliabi10 <141254104+sdzliabi10@users.noreply.github.com>
2023-10-28 06:19:43 +0700

    commit

---
+++ TI_RegPagi_22205024_latihan18_Kelipatan.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tugaspert6;
+
+
+/**
+ *
+ * @author Administrator
+ * Nama : Sofiullah Abi Sadzili
+ * Nim  : 22205024
+ * Prodi : Teknik Informatika
+ * Deskripsi Program : Program ini menampilkan kelipatan bilangan3,5 sampai 35
+ */
+public class TI_RegPagi_22205024_latihan18_Kelipatan {
+    public static void main(String[] args) {
+         for (double i = 3.5; i <= 35; i += 3.5) {
+            if (i % 3 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+    
+    
